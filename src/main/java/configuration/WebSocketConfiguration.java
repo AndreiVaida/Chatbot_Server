b29b5fdcff5efd45da2b2631d83199b9efdec345
AndreiVaida <andrei_vd2006@yahoo.com>
2019-03-17 15:42:51 +0200

    WS working (just connecting part).

--- src/main/java/configuration/WebSocketConfiguration.java
+++ src/main/java/configuration/WebSocketConfiguration.java
@@ -9,15 +9,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
-
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/socket")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker("/notifications");
     }
 }
--- src/main/java/controllers/WebSocketController.java
+++ src/main/java/controllers/WebSocketController.java
@@ -1,17 +1,23 @@
 package controllers;
 
-import dtos.MessageDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 
 @Controller
-public class WebSocketController extends AbstractController {
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public MessageDto greeting(MessageDto message) throws Exception {
-        return new MessageDto(1L, 2L, "Hello, " + HtmlUtils.htmlEscape(message.getMessage()) + "!", null);
+public class WebSocketController {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    public WebSocketController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    @MessageMapping("/send/notification")
+    public void onReceivedMessage(String message) {
+        this.simpMessagingTemplate.convertAndSend("/notifications", message);
     }
 
 }
